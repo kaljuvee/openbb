7a101653f91230a334806ef93d06519712111cfa
Julian Kaljuvee <kaljuvee@gmail.com>
2023-08-22 13:54:51 +0300

    Update openbb_demo.py

--- streamlit/openbb_demo.py
+++ streamlit/openbb_demo.py
@@ -6,13 +6,12 @@ from openbb_terminal.sdk import openbb
 #df_daily = openbb.stocks.load(symbol = 'APG1L.VS')
 
 st.title('Sample OpenBB Output')
-st.subtitle('Balance Sheet')
-
+st.header('Balance sheet')
 balance_sheet = openbb.stocks.fa.balance("APG1L.VS", source="YahooFinance")
 
 st.table(balance_sheet.head())
 
-st.subtitle('Balance Sheet')
+st.header('Balance sheet')
 fig = openbb.stocks.candle(
     data = openbb.stocks.load(symbol = 'APG1L.VS', start_date = '2023-06-01', weekly = True, verbose = False),
     symbol = "APB Apranga - Weekly"
